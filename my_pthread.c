e4f34207ef45dfffc93f7fe788094f160a0c6cdd
Jay <jay@Jays-MacBook-Pro.local>
2018-03-12 12:32:50 +0900

    kkkk

--- my_pthread.c
+++ my_pthread.c
@@ -16,7 +16,7 @@ typedef struct __myret_t {
 void *mythread(void *arg) {
 	myarg_t *m = (myarg_t *) arg;
 	printf("%d %d\n", m->a, m->b);
-	myret_t *r = Malloc(sizeof(myret_t));
+	myret_t *r = malloc(sizeof(myret_t));
 	r->x = 1;
 	r->y = 2;
 	return (void *) r;
@@ -27,8 +27,8 @@ int main(int argc, char *argv[]) {
 	myret_t *m;
 
 	myarg_t args = {10, 20};
-	Pthread_create(&p, NULL, mythread, &args);
-	Pthread_join(p, (void **) &m);
+	pthread_create(&p, NULL, mythread, &args);
+	pthread_join(p, (void **) &m);
 	printf("returned %d %d\n", m->x, m->y);
 	free(m);
 	return 0;
